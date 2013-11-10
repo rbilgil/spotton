55053d7ecf59e146d9dc272a672177511501cc9a
hakJav <hakjav@googlemail.com>
2013-11-10 05:03:35 +0000

    YEP

--- web/js/script.js
+++ web/js/script.js
@@ -60,7 +60,7 @@ function upVote(id){
 }
 
 function newUser(){
-	location.id = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+	localStorage.id = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
 						var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
 						return v.toString(16);
 					});
